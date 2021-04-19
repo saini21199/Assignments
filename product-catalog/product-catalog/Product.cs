28b8ad6175636a92d18ffc828db3565287848754
saini21199 <sachinsainiandromeda007@gmail.com>
2021-04-19 08:00:37 +0530

    added structure and some functionality

---
+++ product-catalog/product-catalog/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace product_catalog
+{
+    public class Product
+    {
+        public static int ID;
+
+        public string productName;
+
+        public string productManufacturer;
+
+        public char[] productCode;
+
+        public Category Categories;
+
+        public string productDescription;
+
+        public string SellingPrice;
+    }
+}
